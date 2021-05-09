eb68ecf79fa4ae9c90088c0e3ea996567f564da4
Saurav Singh <saurav1199.singh@gmail.com>
2021-05-09 11:23:53 +0530

    Create faster_rcnn_r50_fpn_1x_rp2.py

---
+++ configs/faster_rcnn/faster_rcnn_r50_fpn_1x_rp2.py
@@ -0,0 +1,46 @@
+_base_ = "./faster_rcnn_r50_fpn_1x_coco.py"
+
+dataset_type = 'CocoDataset'
+classes = ("Ignore", "Pedestrian", "People", "Bicycle", "Car", "Van", "Truck", "Tricycle", "Awning-tricycle", "Bus", "Motor", "Others")
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file='VisDrone2019-DET-train/annotations.json',
+        img_prefix='VisDrone2019-DET-train/images'),
+    val=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file='VisDrone2019-DET-val/annotations.json',
+        img_prefix='VisDrone2019-DET-val/images'),
+    test=dict(
+        type=dataset_type,
+        # explicitly add your class names to the field `classes`
+        classes=classes,
+        ann_file='VisDrone2019-DET-test/annotations.json',
+        img_prefix='VisDrone2019-DET-test/images'))
+
+# 2. model settings
+
+# explicitly over-write all the `num_classes` field from default 80 to 5.
+model = dict(
+    roi_head=dict(
+        bbox_head=[
+            dict(
+                type='Shared2FCBBoxHead',
+                # explicitly over-write all the `num_classes` field from default 80 to 5.
+                num_classes=12),
+            dict(
+                type='Shared2FCBBoxHead',
+                # explicitly over-write all the `num_classes` field from default 80 to 5.
+                num_classes=12),
+            dict(
+                type='Shared2FCBBoxHead',
+                # explicitly over-write all the `num_classes` field from default 80 to 5.
+                num_classes=12)],
+    # explicitly over-write all the `num_classes` field from default 80 to 5.
+    mask_head=dict(num_classes=12)))
